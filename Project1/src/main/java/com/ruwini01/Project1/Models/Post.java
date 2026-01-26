c662d568d6bf794cc23ed45cc63f33a947246226
ruwini01 <12345tharanga12345@gmail.com>
2026-01-26 09:05:10 +0530

    Post Model Created

---
+++ Project1/src/main/java/com/ruwini01/Project1/Models/Post.java
@@ -0,0 +1,30 @@
+package com.ruwini01.Project1.Models;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Post {
+    
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String title;
+
+    @Column(columnDefinition="TEXT")
+    private String body;
+
+    private LocalDateTime createdAt;
+
+}
