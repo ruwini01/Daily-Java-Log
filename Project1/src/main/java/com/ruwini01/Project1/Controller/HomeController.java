eacd8ff532d8ae5bff48b398537fc32a20904da5
ruwini01 <12345tharanga12345@gmail.com>
2026-01-30 12:31:53 +0530

    Add home controller to list posts

--- Project1/src/main/java/com/ruwini01/Project1/Controller/HomeController.java
+++ Project1/src/main/java/com/ruwini01/Project1/Controller/HomeController.java
@@ -1,14 +1,25 @@
 package com.ruwini01.Project1.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.ruwini01.Project1.Models.Post;
+import com.ruwini01.Project1.Services.PostService;
+
 @Controller
 public class HomeController {
+
+    @Autowired
+    private PostService postService;
     
     @GetMapping("/")
     public String home(Model model){
+        List<Post> posts = postService.getAllPosts();
+        model.addAttribute("posts", posts);
         return "home";
     }
 
